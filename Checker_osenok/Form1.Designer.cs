c5ff13bdb73b27031b698cb5c738a6c1f26b088d
coolhacker13 <33355745+coolhacker13@users.noreply.github.com>
2019-02-06 23:02:40 +0300

    123

--- Checker_osenok/Form1.Designer.cs
+++ Checker_osenok/Form1.Designer.cs
@@ -28,13 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(966, 505);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(966, 505);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.Text = "Cheker_osenok";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Checker_osenok/Form1.cs
+++ Checker_osenok/Form1.cs
@@ -1,20 +1,119 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net;
 
 namespace Checker_osenok
 {
     public partial class Form1 : Form
     {
+        public List<string> lessons = new List<string>();
+        public List<string> marks = new List<string>();
+        public string[] result;
         public Form1()
         {
             InitializeComponent();
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                var html = File.ReadAllText(openFileDialog.FileName, Encoding.Default);
+                var res = Regex.Matches(html, @"<td class=\Ds2\D>.*</td>");
+                if (res.Count == 0)
+                    richTextBox1.Text = "net";
+                result = res.Cast<Match>().Select(x => x.Value).ToArray();
+                Start_Work(result);
+            }
+            
+        }
+
+        void Start_Work(string[] result)
+        {
+            Regex pattern = new Regex(@"<strong class=\Du\D>\w+");
+            Regex pattern_marks = new Regex(@"data-num=\D\d\D>\d?");
+            for (int i = 0; i < result.Count(); i++)
+            {
+                MatchCollection matches = pattern.Matches(result[i]);
+                MatchCollection matcher_marks = pattern_marks.Matches(result[i]);
+                if (matches.Count > 0)
+                    Print(matches);
+                else
+                    richTextBox1.Text = "Совпадений не найдено";
+                if (matcher_marks.Count > 0)
+                    Print_marks(matcher_marks);
+            }
+        }
+
+      void Print(MatchCollection matches)
+        {
+            foreach (Match match in matches)
+            {
+                var str = match.Groups[0].ToString();
+                richTextBox1.Text += str.Substring(18, str.Length-18);
+                lessons.Add(str.Substring(18, str.Length-18));
+                richTextBox1.Text += "\r\n";
+            }
+        }
+
+        void Print_marks(MatchCollection matches)
+        {
+            marks.Add(null);
+            foreach (Match match in matches)
+            {
+                var str = match.Groups[0].ToString();
+                richTextBox1.Text += str.Substring(13, str.Length - 13);
+                marks[marks.Count-1] += str.Substring(13, str.Length - 13);
+                richTextBox1.Text += " ";
+            }
+            richTextBox1.Text += Average();
+            richTextBox1.Text += "\r\n ";
+        }
+
+      public string Average()
+        {
+            float av = 0;
+            string OutPut = "";
+                for (int i2=0; i2<marks[marks.Count-1].Length; i2++)
+                {
+                    av += (int)Char.GetNumericValue(marks[marks.Count-1][i2]);
+                }
+            av /= marks[marks.Count - 1].Length;
+            OutPut += " ср. балл:" + av;
+            if (marks[marks.Count - 1].Length < 3)
+                OutPut += "; не хватает оценок:" + (3 - marks[marks.Count - 1].Length);
+            if (Math.Round(av) < 5)
+            {
+                float res2 = (4.6f - av) * marks[marks.Count - 1].Length;
+                av = res2 + 1;
+            }
+            else av = 0;
+            OutPut += "; до \"Отличного\" нужно еще пятерок: " + Math.Round(av);
+            return OutPut;
+        }
+                      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //System.Net.WebRequest reqGET = System.Net.WebRequest.Create(@"https://schools.school.mosreg.ru/marks.aspx?school=49652&index=7&tab=period&homebasededucation=False");
+            //System.Net.WebResponse resp = reqGET.GetResponse();
+            //System.IO.Stream stream = resp.GetResponseStream();
+            //System.IO.StreamReader sr = new System.IO.StreamReader(stream);
+            //string s = sr.ReadToEnd();
+            //File.WriteAllText("321.txt", s);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keys.Enter == e.KeyCode && richTextBox1.Text.Length>10)
+            {
+
+            }
         }
     }
 }
